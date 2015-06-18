59e2e2838afd3b48352f171c8ff69685769a5ed3
conan <conan.cook@gmail.com>
2015-06-18 19:22:08 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject dirty-xml "0.1.0-SNAPSHOT"
+
+  :description "Quick-and-dirty conversions between EDN and XML"
+
+  :url "https://github.com/conan/dirty-xml"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]])
---
+++ test/dirty_xml/core_test.clj
@@ -0,0 +1,7 @@
+(ns dirty-xml.core-test
+  (:require [clojure.test :refer :all]
+            [dirty-xml.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
